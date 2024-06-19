f065c1bdda078411b421612d5f3eddfa77d508b4
Rajdip Bhattacharya <agentR47@gmail.com>
2024-06-19 17:47:13 +0530

    feat: Add GitHub profile fetching option (#123)

--- src/user/profile-fetcher/profile-fetcher.factory.ts
+++ src/user/profile-fetcher/profile-fetcher.factory.ts
@@ -1,10 +1,13 @@
 import { BadRequestException } from '@nestjs/common';
 import { LinkedInProfileFetcher } from './linkedin.profile-fetcher';
+import { GitHubProfileFetcher } from './github.profile-fetcher';
 
 export class ProfileFetcherFactory {
   generateProfileFetcher(profileUrl: string) {
     if (profileUrl.startsWith('https://www.linkedin.com')) {
       return new LinkedInProfileFetcher(profileUrl);
+    } else if (profileUrl.startsWith('https://github.com')) {
+      return new GitHubProfileFetcher(profileUrl);
     }
     throw new BadRequestException('Unsupported profile URL');
   }
